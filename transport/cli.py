48bf7172c23d2b4591ab8dd076f99927be118481
yinheli <me@yinheli.com>
2021-01-20 18:18:24 +0800

    if not args, print help

--- transport/cli.py
+++ transport/cli.py
@@ -5,7 +5,7 @@ import click
 from transport.transport import Transport
 
 
-@click.command()
+@click.command(no_args_is_help=True)
 @click.option("-f", "--excel", default="贴源层表清单梳理.xlsx", type=click.Path(exists=True), help="梳理好的 excel 文件完整路径")
 @click.option("-s", "--sql", default="gdm_table.hql", type=click.Path(exists=True), help="系统导出的 hql 文件，作为基础模板")
 @click.option("-o", "--out", default=os.getcwd(), type=click.Path(exists=True), help="输出文件夹，默认输出到当前文件夹")
