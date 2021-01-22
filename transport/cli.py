8b487a9bdb8029e93d60d09ba781d5aafd647803
yinheli <me@yinheli.com>
2021-01-22 19:03:38 +0800

    add encoding option

--- transport/cli.py
+++ transport/cli.py
@@ -8,8 +8,9 @@ from transport.transport import Transport
 @click.command(no_args_is_help=True)
 @click.option("-f", "--excel", default="贴源层表清单梳理.xlsx", type=click.Path(exists=True), help="梳理好的 excel 文件完整路径")
 @click.option("-s", "--sql", default="gdm_table.hql", type=click.Path(exists=True), help="系统导出的 hql 文件，作为基础模板")
+@click.option("-e", "--encoding", default='UTF-8', help="hql 文件编码，默认为 UTF-8")
 @click.option("-o", "--out", default=os.getcwd(), type=click.Path(exists=True), help="输出文件夹，默认输出到当前文件夹")
-def main(excel: str, sql: str, out: str):
+def main(excel: str, sql: str, out: str, encoding: str):
     """一个简单批处理工具，输入整理好的 excel 和 hql 表结构，基于模板输出 ddl 和 procedure"""
-    transport = Transport()
-    transport.handle(excel, sql, out)
+    transport = Transport(excel, sql, out, encoding=encoding)
+    transport.handle()
